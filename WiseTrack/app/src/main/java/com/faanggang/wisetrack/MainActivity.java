9192fe1a7435aa3972d542038a938c9eb5b0bf81
celina <csheng2@ualberta.ca>
2021-03-04 23:40:39 -0700

    setContentView to main_menu.xml temporarily. Will implement publishButton.setOnClickListener() in publish_experiment_implementations branch

--- WiseTrack/app/src/main/java/com/faanggang/wisetrack/MainActivity.java
+++ WiseTrack/app/src/main/java/com/faanggang/wisetrack/MainActivity.java
@@ -3,12 +3,36 @@ package com.faanggang.wisetrack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+
+    ListView experimentList;
+    ArrayAdapter<Experiment> experimentAdapter;
+    ArrayList<Experiment> experimentDataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.main_menu);  // EDIT LATER: display main_menu later
+                                                // create class to handle menu button clicks?
+
+        Button publishButton = findViewById(R.id.menuPublish_button);
+        publishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TO DO
+            }
+        });
+
+
     }
 }
