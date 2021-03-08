d3dcc6c2f6e0253db8176a121aee330a66804318
zloopo <seliayeu@ualberta.ca>
2021-03-08 10:20:20 -0700

    integrate authentication functionality into current StartActivity

--- WiseTrack/app/src/main/java/com/faanggang/wisetrack/MainActivity.java
+++ WiseTrack/app/src/main/java/com/faanggang/wisetrack/MainActivity.java
@@ -12,16 +12,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
-import com.faanggang.wisetrack.userauth.NewUserActivity;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity{
@@ -29,7 +19,6 @@ public class MainActivity extends AppCompatActivity{
     ListView experimentList;
     ArrayAdapter<Experiment> experimentAdapter;
     ArrayList<Experiment> experimentDataList;
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +31,6 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.main_menu);  // EDIT LATER: display main_menu later
                                                 // create class to handle menu button clicks?
 
-        mAuth = FirebaseAuth.getInstance();
 
         Button publishButton = findViewById(R.id.menuPublish_button);
         publishButton.setOnClickListener(new View.OnClickListener() {
@@ -52,20 +40,5 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
-    }
-
-    // code modified from code samples found at https://firebase.google.com/docs/auth/android/start
-    // which are licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser == null){
-            Intent intent = new Intent(this, NewUserActivity.class);
-            startActivity(intent);
-        } else {
-            Log.w("epic", currentUser.getUid());
-        }
     }
 }
