c8ab1441d1920e29001e86468a69c965b728aead
celina <csheng2@ualberta.ca>
2021-03-08 20:57:57 -0700

    last activity before publishing the experiment

---
+++ WiseTrack/app/src/main/java/com/faanggang/wisetrack/PublishExperimentActivity4.java
@@ -0,0 +1,14 @@
+package com.faanggang.wisetrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PublishExperimentActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish_experiment4);
+    }
+}
