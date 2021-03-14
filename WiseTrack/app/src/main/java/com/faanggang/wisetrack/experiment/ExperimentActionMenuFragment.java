02ee319ab4097b8251de0ed0bfe11f67fe8cb0c6
Ava <ava.gyx@gmail.com>
2021-03-14 16:08:19 -0600

    feat: added experiment action menu fragment

--- WiseTrack/app/src/main/java/com/faanggang/wisetrack/experiment/ExperimentActionMenuFragment.java
+++ WiseTrack/app/src/main/java/com/faanggang/wisetrack/experiment/ExperimentActionMenuFragment.java
@@ -1,10 +1,17 @@
 package com.faanggang.wisetrack.experiment;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+
+import com.faanggang.wisetrack.R;
 
 public class ExperimentActionMenuFragment extends DialogFragment {
     private TextView subscribe;
@@ -17,7 +24,8 @@ public class ExperimentActionMenuFragment extends DialogFragment {
     private TextView trials;
 
     @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.experiment_action_menu_fragment, null);
     }
 }
--- WiseTrack/app/src/main/java/com/faanggang/wisetrack/experiment/ViewExperimentActivity.java
+++ WiseTrack/app/src/main/java/com/faanggang/wisetrack/experiment/ViewExperimentActivity.java
@@ -57,7 +57,7 @@ public class ViewExperimentActivity extends AppCompatActivity {
         ExperimentActionMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                new ExperimentActionMenuFragment().show(getSupportFragmentManager(), "experiment action menu");
             }
         });
     }
