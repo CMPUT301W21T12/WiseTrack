96c30ca10ec3aa34d02b807a29f81da519adff6a
Ava <ava.gyx@gmail.com>
2021-03-14 19:50:11 -0600

    fix: changed experiment action menu to opening on short-click instead of long-click - improvement on user friendliness

--- WiseTrack/app/src/main/java/com/faanggang/wisetrack/experiment/ViewExperimentActivity.java
+++ WiseTrack/app/src/main/java/com/faanggang/wisetrack/experiment/ViewExperimentActivity.java
@@ -73,8 +73,7 @@ public class ViewExperimentActivity extends AppCompatActivity {
         ExperimentActionMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new ExperimentActionMenuFragment().show(getSupportFragmentManager(), "experiment action menu");
-                v.showContextMenu();
+                v.showContextMenu();  // Note: default is long clicking to open action menu
             }
         });
 
