afa1927337727c6b6339f0f15ff7c73145398ecb
Joseph Dimaano <jdimaano@ualberta.ca>
2021-03-16 17:48:46 -0600

    Added front end functionality for stats report

--- WiseTrack/app/src/main/java/com/faanggang/wisetrack/statsManager/StatManager.java
+++ WiseTrack/app/src/main/java/com/faanggang/wisetrack/statsManager/StatManager.java
@@ -2,7 +2,7 @@ package com.faanggang.wisetrack.statsManager;
 
 /**
  * Manage and handle all statistic related methods for experiments
- *
+ * To be used in all activity files
  * Important things to keep in mind:
  * Trial type is determined by an int via "trialType"
  * 0 - Count
@@ -11,16 +11,29 @@ package com.faanggang.wisetrack.statsManager;
  * 3 - Measurements
  */
 public class StatManager {
+    public StatReport currentTrialReport = new StatReport(0,0,0, new float[]{0, 0, 0});
+    //private StatHistogram currentTrialHistogram;
+    //private StatPlot currentTrialPlot;
     public StatManager () {
 
     }
+    /**
+     * Calculate the following:
+     * Median , Mean, Std. Deviation and quartiles
+     */
     public void generateStatReport() {
-        /**
-         * Calculate the following:
-         * Median , Mean, Std. Deviation and quartiles
-         */
+        float[] trialData = new float[]{42,3,4,7,18,21,26,44,69,10}; // Test array
+
+        // grab trial data from trial as a float array
+        float mean = currentTrialReport.calculateMean(trialData);
+        float median = currentTrialReport.calculateMedian(trialData);
+        double stdev = currentTrialReport.calculateStdev(trialData);
+        float[] quartiles = currentTrialReport.calculateQuartiles(trialData);
+        float iqRange = currentTrialReport.calculateInterquartileRange(trialData);
+
+
     }
-    public void displayStatReport() {
+    public void displayStatReport() {// What is this for ?
         /**
          * Displays the following (needs to change based on trial to get more digestable information)
          * Default is : Median , Mean , Standard Deviation and quartiles
