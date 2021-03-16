18b3f51aecc81e712618d134a83101f7ade5ad45
zloopo <seliayeu@ualberta.ca>
2021-03-16 12:57:36 -0600

    add Mockito unit testing for SearchManager and add updated javadocs

--- WiseTrack/app/src/main/java/com/faanggang/wisetrack/search/SearchManager.java
+++ WiseTrack/app/src/main/java/com/faanggang/wisetrack/search/SearchManager.java
@@ -20,7 +20,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * This class controls searching the database.
+ * This class controls searching the database for experiments.
  */
 
 public class SearchManager {
@@ -40,9 +40,11 @@ public class SearchManager {
      * current searcher.
      * @param searcher
      * searcher is the activity that will be using this instance of SearchManager
+     * @param db
+     * injected database to send requests to
      */
-    public SearchManager(Searcher searcher) {
-        db = FirebaseFirestore.getInstance();
+    public SearchManager(Searcher searcher, FirebaseFirestore db) {
+        this.db = db;
         this.searcher = searcher;
     }
 
@@ -56,17 +58,9 @@ public class SearchManager {
 
         CollectionReference experiments = db.collection("Experiments");
         ArrayList<Experiment> results;
-        Log.w("SEARCH", "starting search for query");
 
-        ArrayList<String> queryKeywords = new ArrayList<>();
-        queryKeywords.addAll(Arrays.asList(query.split(" ")));
-        // make all of the capital
-        for (int i = 0; i < queryKeywords.size(); i++) {
-            queryKeywords.set(i, queryKeywords.get(i).toUpperCase());
-        }
-        if (queryKeywords.size() > 10) {
-            queryKeywords.subList(0, 10);
-        }
+        ArrayList<String> queryKeywords = getKeywordsFromString(query);
+
         db.collection("Experiments").whereArrayContainsAny("keywords", queryKeywords)
                 .orderBy("datetime")
                 .get()
@@ -89,11 +83,27 @@ public class SearchManager {
                         }
                         searcher.onSearchSuccess(searchResults);
                     } else {
-                        Log.w("SEARCH", "EXCEPTION: " + task.getException().toString());
                     }
                 })
                 .addOnFailureListener(e -> {
-                    Log.w("SEARCH", "FAILURE: " + e.toString());
                 });
+    }
+
+    /**
+     * This method creates an ArrayList of keywords from a passed string.
+     * @param query
+     * query is a string that contains keywords
+     */
+    public ArrayList<String> getKeywordsFromString(String query) {
+        ArrayList<String> queryKeywords = new ArrayList<>();
+        queryKeywords.addAll(Arrays.asList(query.split(" ")));
+        // make all of the capital
+        for (int i = 0; i < queryKeywords.size(); i++) {
+            queryKeywords.set(i, queryKeywords.get(i).toUpperCase());
+        }
+        if (queryKeywords.size() > 10) {
+            queryKeywords.subList(0, 10);
+        }
+        return queryKeywords;
     }
 }
--- WiseTrack/app/src/test/java/com/faanggang/wisetrack/SearchManagerTest.java
+++ WiseTrack/app/src/test/java/com/faanggang/wisetrack/SearchManagerTest.java
@@ -1,15 +1,88 @@
 package com.faanggang.wisetrack;
 
+import com.faanggang.wisetrack.publish.PublishingController;
+import com.faanggang.wisetrack.search.SearchManager;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.internal.matchers.Null;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
+
 public class SearchManagerTest {
+    private static MockSearcher mockSearcher;
+    private static FirebaseFirestore mockDb;
+    private static CollectionReference mockCollectionRef;
+    private static Query mockQuery;
+    private static ArrayList<String> keywords;
+    private static Task mockTask;
+
+    @BeforeClass
+    public static void initializeObject() {
+        mockDb = Mockito.mock(FirebaseFirestore.class);
+        mockCollectionRef = Mockito.mock(CollectionReference.class);
+        mockQuery = Mockito.mock(Query.class);
+        mockTask = Mockito.mock(Task.class);
+
+        mockSearcher = new MockSearcher(mockDb);
+        keywords = new ArrayList<>();
+        keywords.add("TEST");
+        keywords.add("EXPERIMENTS");
+
+        Mockito.when(mockDb.collection("Experiments"))
+                .thenReturn(mockCollectionRef);
+        Mockito.when(mockCollectionRef.whereArrayContainsAny("keywords", keywords))
+                .thenReturn(mockQuery);
+        Mockito.when(mockQuery.orderBy("datetime"))
+                .thenReturn(mockQuery);
+        Mockito.when(mockQuery.get())
+                .thenReturn(mockTask);
+        Mockito.when(mockTask.addOnCompleteListener(Mockito.any()))
+                .thenReturn(mockTask);
+        Mockito.when(mockTask.addOnFailureListener(Mockito.any()))
+                .thenReturn(mockTask);
+    }
+
     @Test
-    public void testUtilities() {
-        assertEquals(true, true);
+    public void testGetKeywords() {
+        String queryString = "awesome Best Experiments";
+        String[] expectedReturn = new String[]{"AWESOME", "BEST", "EXPERIMENTS"};
+
+        assertArrayEquals(mockSearcher.getKeywordsFromString(queryString).toArray(), expectedReturn);
+    }
+
+    @Before
+    public void cleanUp() {
+        Mockito.reset();
+    }
+
+    @Test
+    public void testSearchByKeyword() {
+        mockSearcher = new MockSearcher(mockDb);
+        mockSearcher.makeSearchRequest("Test Experiments");
+
+        Mockito.verify(mockDb, Mockito.times(2))
+                .collection("Experiments");
+        Mockito.verify(mockCollectionRef, Mockito.times(1))
+                .whereArrayContainsAny("keywords", keywords);
+        Mockito.verify(mockQuery, Mockito.times(1))
+                .orderBy("datetime");
     }
 
 }
