dbc14ce5765b89fc7da01573f47e00c56582a727
dan <dan.seliayeu@gmail.com>
2021-04-04 17:12:53 -0600

    add accredation to code

--- WiseTrack/app/src/main/java/com/faanggang/wisetrack/view/MainMenuActivity.java
+++ WiseTrack/app/src/main/java/com/faanggang/wisetrack/view/MainMenuActivity.java
@@ -108,26 +108,19 @@ public class MainMenuActivity extends AppCompatActivity {
         }
     }
 
+    // adapted from code found https://developer.android.com/training/permissions/requesting
+    // which is licensed under Apache 2.0
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             case 1:
-                // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0 &&
                         grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     geolocationManager.startLocationUpdates();
-                }  else {
-                    // Explain to the user that the feature is unavailable because
-                    // the features requires a permission that the user has denied.
-                    // At the same time, respect the user's decision. Don't link to
-                    // system settings in an effort to convince the user to change
-                    // their decision.
                 }
                 return;
         }
-        // Other 'case' lines to check for other
-        // permissions this app might request.
     }
 
     @Override
