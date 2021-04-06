77bd652ba32bf930649f9f80c453b63b3d27667a
AG <ava.gyx@gmail.com>
2021-04-06 02:10:04 -0600

    feat: added trials recyclerView basic structure

--- WiseTrack/app/src/main/java/com/faanggang/wisetrack/controllers/TrialFetchManager.java
+++ WiseTrack/app/src/main/java/com/faanggang/wisetrack/controllers/TrialFetchManager.java
@@ -1,8 +1,12 @@
 package com.faanggang.wisetrack.controllers;
 
+import android.util.Log;
+
 import com.faanggang.wisetrack.model.executeTrial.BinomialTrial;
 import com.faanggang.wisetrack.model.executeTrial.CountTrial;
+import com.faanggang.wisetrack.model.executeTrial.MeasurementTrial;
 import com.faanggang.wisetrack.model.executeTrial.Trial;
+import com.faanggang.wisetrack.model.experiment.Experiment;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -10,6 +14,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 public class TrialFetchManager {
     private TrialFetcher fetcher;
@@ -25,7 +30,7 @@ public class TrialFetchManager {
     }
 
     /**
-     * This method updates the "Subscription" field of a user on the firebase.
+     * This method queries the Cloud Firestore to find experiments that have been subscribed to by a specific user.
      * @param expID
      * expID is the ID of the experiment that you are adding to.
      */
@@ -41,17 +46,36 @@ public class TrialFetchManager {
                                 .collection("Trials")
                                 .get()
                                 .addOnCompleteListener(task1 -> {
-                                    ArrayList<Trial> trials = new ArrayList<>();
-                                    for (DocumentSnapshot docSnapshot : task1.getResult().getDocuments()) {
-                                        trials.add(createTrial(trialType, docSnapshot));
+                                    if (task1.isSuccessful()) {
+                                        ArrayList<Trial> trials = new ArrayList<>();
+                                        for (DocumentSnapshot docSnapshot : task1.getResult().getDocuments()) {
+                                            Log.w("TRIALS", docSnapshot.getString("result"));
+                                            trials.add(createTrial(
+                                                    trialType,
+                                                    docSnapshot.getLong("result"),
+                                                    docSnapshot.getString("geolocation"),
+                                                    docSnapshot.getString("conductor id"),
+                                                    docSnapshot.getDate("date"))
+                                            );
+                                        }
+                                        fetcher.onSuccessfulFetch(trials);
                                     }
-                                    fetcher.onSuccessfulFetch(trials);
                                 });
                     }
                 });
     }
 
-    public Trial createTrial(int trialType, DocumentSnapshot snapshot) {
-        return new BinomialTrial(1, "text", "text", new Date());
+    public Trial createTrial(int trialType, float trialResult, String trialGeolocation, String conductorID, Date date) {
+        Trial trial;
+        if ((trialType == 0)||(trialType == 2)) {  // count and NNIC type trials
+            trial = new CountTrial((int)trialResult, trialGeolocation, conductorID, date);
+        } else if (trialType == 1) {
+            trial = new BinomialTrial((int)trialResult, trialGeolocation, conductorID, date);
+        } else if (trialType == 3) {
+            trial = new MeasurementTrial(trialResult, trialGeolocation, conductorID, date);
+        } else {
+            trial = null;  // this might not be necessary
+        }
+        return trial;
     }
 }
