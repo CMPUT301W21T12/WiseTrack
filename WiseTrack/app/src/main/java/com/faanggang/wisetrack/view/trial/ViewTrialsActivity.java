aa47e5ee8aef2a2361c927188f6eec9e0a148051
AG <ava.gyx@gmail.com>
2021-04-08 17:00:36 -0600

    staging to merge with master

--- WiseTrack/app/src/main/java/com/faanggang/wisetrack/view/trial/ViewTrialsActivity.java
+++ WiseTrack/app/src/main/java/com/faanggang/wisetrack/view/trial/ViewTrialsActivity.java
@@ -13,6 +13,7 @@ import com.faanggang.wisetrack.model.experiment.Experiment;
 import com.faanggang.wisetrack.view.adapters.TrialAdapter;
 import com.faanggang.wisetrack.view.adapters.TrialItemView;
 import com.faanggang.wisetrack.view.comment.AddCommentFragment;
+import com.faanggang.wisetrack.view.user.ViewOtherActivity;
 import com.google.api.LogDescriptor;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -42,7 +43,7 @@ public class ViewTrialsActivity extends AppCompatActivity implements TrialFetchM
         setContentView(R.layout.activity_view_trials);
 
         trialFetchManager = new TrialFetchManager(this);
-        trials = new ArrayList<>();
+        trials = new ArrayList<Trial>();
         trialAdapter = new TrialAdapter(trials, this, this);
 
         recyclerView = findViewById(R.id.recyclerview_view_trials);
@@ -55,13 +56,12 @@ public class ViewTrialsActivity extends AppCompatActivity implements TrialFetchM
         expID = extras.getString("EXP_ID");
 
         trialFetchManager.fetchTrials(expID);
-
     }
 
     /**
      * This method makes the RecyclerView display trials of current experiment
      * @param results
-     *     results are trials to display which were successfully fetched from Cloud Firestore.
+     *     results are trials to display which were successfully fetched from Cloud Firebase.
      */
     @Override
     public void onSuccessfulFetch(ArrayList<Trial> results) {
@@ -86,7 +86,8 @@ public class ViewTrialsActivity extends AppCompatActivity implements TrialFetchM
     public boolean onMenuItemClick(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.view_profile_option:
-                Toast.makeText(this, "view profile option selected", Toast.LENGTH_SHORT).show();
+                Intent viewUserIntent = new Intent(this, ViewOtherActivity.class);
+                //viewUserIntent.putExtra("UID")
                 return true;
             case R.id.block_user_option:
                 Toast.makeText(this, "block user option selected", Toast.LENGTH_SHORT).show();
