9fe9ab0f026152725abf31b3de9630fbe1ddba8a
Ben Wang <xiaoyu.w.99@gmail.com>
2021-04-07 22:12:50 -0600

    javadocs + small code change

--- WiseTrack/app/src/main/java/com/faanggang/wisetrack/controllers/QRCodeManager.java
+++ WiseTrack/app/src/main/java/com/faanggang/wisetrack/controllers/QRCodeManager.java
@@ -21,6 +21,9 @@ import java.util.Map;
 import static android.graphics.Color.BLACK;
 import static android.graphics.Color.WHITE;
 
+/**
+ * QRCodeManager is primarily concerned with the handing of String to QR Codes and Codes back to Strings.
+ */
 public class QRCodeManager {
     private FirebaseFirestore db;
 
@@ -50,14 +53,26 @@ public class QRCodeManager {
         this.barcodeListener = listener;
     }
 
+    /**
+     * Takes in a string, and dimensions and returns a bitmap that represents the string.
+     * Source: Adapter from Alexander Farber, https://stackoverflow.com/a/30529128
+     * @param str
+     * String to be encoded.
+     * @param qrWidth
+     * Width of the final bitmap.
+     * @param qrHeight
+     * Height of the final bitmap.
+     * @return
+     * Returns a bitmap representing the string input.
+     */
     public Bitmap stringToBitmap(String str, int qrWidth, int qrHeight) {
         BitMatrix result;
         try {
             result = new MultiFormatWriter().encode(str,
                     BarcodeFormat.QR_CODE, qrWidth, qrHeight, null);
         } catch (IllegalArgumentException | WriterException iae) {
-            // Unsupported format
-            return null;
+            Log.e("QR", iae.toString());
+            result = null;
         }
         int w = result.getWidth();
         int h = result.getHeight();
@@ -73,6 +88,16 @@ public class QRCodeManager {
         return bitmap;
     }
 
+    /**
+     * Generates a new qr code for an experiment if it does not exist already. If it does, then retrives that code.
+     * It will then set teh ImageView to display the QR code.
+     * @param expID
+     * ID of the Experiment
+     * @param trialResult
+     * Result that the QR indicates
+     * @param image
+     * ImageView that will be changed to the QR code.
+     */
     public void requestCodesForExperiment(String expID, int trialResult, ImageView image){
         db.collection("QRCodes").whereEqualTo("expID", expID)
             .whereEqualTo("trialResult", trialResult)
@@ -85,7 +110,6 @@ public class QRCodeManager {
                 } else {
                     code = doc.get(0).getId();
                 }
-                //PUT REPLACEMENT HERE
                 Bitmap bitmap = stringToBitmap(code,200,200);
                 image.setImageBitmap(bitmap);
             } else {
@@ -94,6 +118,11 @@ public class QRCodeManager {
         });
     }
 
+    /**
+     * Finds out if a scanned QRcode exists and points to a valid experiment. If it does, it will call the scanListener interface function.
+     * @param code
+     * String that is read from the QR code.
+     */
     public void readCode(String code) {
         db.collection("QRCodes").document(code).get()
             .addOnCompleteListener( task -> {
@@ -122,6 +151,11 @@ public class QRCodeManager {
             });
     }
 
+    /**
+     * Finds out if a scanned bar code exists and points to a valid experiment. If it does, it will call the barcodeListener interface function.
+     * @param code
+     * String read from barcode
+     */
     public void checkBarcode(String code){
         db.collection("QRodes").document(code).get()
             .addOnCompleteListener( task -> {
@@ -138,6 +172,17 @@ public class QRCodeManager {
             });
     }
 
+    /**
+     * Registers a barcode to be associated with an experiment trial result.
+     * @param expID
+     * ID of the experiment.
+     * @param trialResult
+     * Result of the Trial the bar code will be associated with.
+     * @param id
+     * String of the barcode to save.
+     * @param userID
+     * ID of the user that the barcode will belong to.
+     */
     public void addBarcode(String expID, int trialResult, String id, String userID){
         Map<String, Object> codeMap = new HashMap<>();
         codeMap.put("expID", expID);
@@ -148,6 +193,14 @@ public class QRCodeManager {
         newCode.set(codeMap);
     }
 
+    /**
+     * Registers a QR code to be associated with an experiment trial result. Unlike barcode, this QR code is public.
+     * @param expID
+     * ID of the experiment.
+     * @param trialResult
+     * Result of the trial the bar code will be associated with.
+     * @return
+     */
     private String addQRCode(String expID, int trialResult){
         Map<String, Object> codeMap = new HashMap<>();
         codeMap.put("expID", expID);
